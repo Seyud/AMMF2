60da793bbc7694aed79795fc409470d48b7beab5
Seyud <2159455958@qq.com>
2025-04-26 16:03:42 +0800

    基本配置

--- module_settings/config.sh
+++ module_settings/config.sh
@@ -1,14 +1,14 @@
 #!/system/bin/sh
-action_id="Module_ID"
-action_name="Module Name"
-action_author="Module Author"
-action_description="Module Description"
+action_id="dimensity_hybrid_governor"
+action_name="天玑GPU混合调速器1000Plus"
+action_author="嘟嘟ski&酷安@瓦力喀"
+action_description="适用于Mediatek Dimensity 1000+的GPU辅助调速器，改善在高画质游戏中的功耗和性能表现（酷安@瓦力喀适配天玑1000+）"
 
-print_languages="en"                   # Default language for printing
+print_languages="zh"                   # Default language for printing
 
 # About Install
 magisk_min_version="25400"             # Minimum required version of Magisk
 ksu_min_version="11300"                # Minimum compatible version of KernelSU
 ksu_min_kernel_version="11300"         # Minimum compatible kernel version of KernelSU
 apatch_min_version="10657"             # Minimum compatible version of APatch
-ANDROID_API="26"                      # Minimum required Android API level
+ANDROID_API="29"                      # Minimum required Android API level
