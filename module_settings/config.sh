763afab204037475d9fd3863fe5fbc1020a30da3
Seyud <2159455958@qq.com>
2025-04-26 16:58:44 +0800

    fix: 修复Magisk最低版本要求为20400

--- module_settings/config.sh
+++ module_settings/config.sh
@@ -7,7 +7,7 @@ action_description="适用于Mediatek Dimensity 1000+的GPU辅助调速器，改
 print_languages="zh"                   # Default language for printing
 
 # About Install
-magisk_min_version="25400"             # Minimum required version of Magisk
+magisk_min_version="20400"             # Minimum required version of Magisk
 ksu_min_version="11300"                # Minimum compatible version of KernelSU
 ksu_min_kernel_version="11300"         # Minimum compatible kernel version of KernelSU
 apatch_min_version="10657"             # Minimum compatible version of APatch
